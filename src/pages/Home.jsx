dd2126b4c6796f285ae7a4f6b97f292ff4d26c4a
Caner Yakupoğlu <caneryakupoglu@outlook.com>
2024-11-11 19:11:23 +0300

    try deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -142,7 +142,7 @@ function Home({ account }) {
         textAlign: 'center',
         paddingTop: '30px' // Navbar ile başlık arasına boşluk
       }}>
-        Active Lotteries
+        Active Lotteriess
       </h1>
       <LotteryList 
         lotteries={lotteries}
